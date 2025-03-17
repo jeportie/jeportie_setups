2e9de0b5ab3426dcd82e2611b446755054242a33
jeportie <jeportie@student.42.fr>
2025-03-17 17:19:28 +0000

    push pause

--- nvjej_docker/install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-touch ~/.cache/docker_nv_reset.flag
-docker build -t jeportie/nvjej:latest .
-./script/install_alias.sh
--- nvjej_docker/update.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-touch ~/.cache/docker_nv_reset.flag
-docker build -t jeportie/nvjej:latest .
